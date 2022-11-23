6fa7346f3b4f1d8e64a03a106fdcdd51cb79d9f8
Okami_PL <szymon.galuszka.114@gmail.com>
2022-11-23 13:27:14 +0100

    update

--- desktopowe/CSharpRS3/CSharpRS3/Program.cs
+++ desktopowe/CSharpRS3/CSharpRS3/Program.cs
@@ -2,6 +2,32 @@
 
 namespace CSharpRS3
 {
+    class Samochod
+    {
+        public static int ilosc = 0;
+        private string marka;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        private double srednieSpalanie;
+        private int liczbSamochodow = 0;
+        public Samochod()
+        {
+
+        }
+        public Samochod(int a)
+        {
+
+        }
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            return 0;
+        }
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
+            return 0;
+        }
+    }
     internal class Program
     {
         static void Main()
