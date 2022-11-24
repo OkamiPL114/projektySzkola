e8e024330aa05ad8a14f9c3433e3c388664151e5
Okami_PL <szymon.galuszka.114@gmail.com>
2022-11-24 09:19:05 +0100

    zapis do pliku i odczytywanie numeru

--- desktopowe/rozbudzWyobraznie/rozbudzWyobraznie/MainWindow.xaml.cs
+++ desktopowe/rozbudzWyobraznie/rozbudzWyobraznie/MainWindow.xaml.cs
@@ -12,18 +12,45 @@ namespace rozbudzWyobraznie
         {
             InitializeComponent();
         }
+        private string NewNoteNumber()
+        {
+            string fileName = "number.txt";
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var numberPath = Path.Combine(path, fileName);
+            if (!File.Exists(numberPath))
+            {
+                File.WriteAllText(numberPath, "1");
+                return "1";
+            }
+            else
+            {
+                string noteNum = File.ReadAllText(numberPath);
+                int noteNumInt = int.Parse(noteNum) + 1;
+                File.WriteAllText(numberPath, noteNumInt.ToString());
+                return noteNumInt.ToString();
+            }
+        }
+        private string GetNoteNumber()
+        {
+            string fileName = "number.txt";
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var numberPath = Path.Combine(path, fileName);
 
+            string noteNum = File.ReadAllText(numberPath);
+            return noteNum.ToString();
+        }
         private void saveNote_Click(object sender, RoutedEventArgs e)
         {
-            string fileName = "text.txt";
+            string fileName = "notes.txt";
             var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
             var notesPath = Path.Combine(path, fileName);
             string text = saveNoteTextBox.Text;
-            File.AppendAllText(notesPath, text);
+            File.AppendAllText(notesPath, $"{NewNoteNumber()}.{text}\n");
         }
 
         private void readNote_Click(object sender, RoutedEventArgs e)
         {
+            string noteNum = GetNoteNumber();
 
         }
     }
