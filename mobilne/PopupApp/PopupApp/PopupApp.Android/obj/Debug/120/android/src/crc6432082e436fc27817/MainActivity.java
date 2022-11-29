c8aba16fa331e318e2981125ec23d2e7b42527db
Okami_PL <szymon.galuszka.114@gmail.com>
2022-11-29 19:37:11 +0100

    nawet nie wiem co to

--- mobilne/PopupApp/PopupApp/PopupApp.Android/obj/Debug/120/android/src/crc6432082e436fc27817/MainActivity.java
+++ mobilne/PopupApp/PopupApp/PopupApp.Android/obj/Debug/120/android/src/crc6432082e436fc27817/MainActivity.java
@@ -20,16 +20,18 @@ public class MainActivity
 	public MainActivity ()
 	{
 		super ();
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("PopupApp.Droid.MainActivity, PopupApp.Android", "", this, new java.lang.Object[] {  });
+		}
 	}
 
 
 	public MainActivity (int p0)
 	{
 		super (p0);
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("PopupApp.Droid.MainActivity, PopupApp.Android", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
 	}
 
 
