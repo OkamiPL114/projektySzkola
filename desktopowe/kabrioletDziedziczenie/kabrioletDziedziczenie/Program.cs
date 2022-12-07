356e188d2d1c6ff7a28d6d9f0a907fcc3bc8fd24
Okami_PL <szymon.galuszka.114@gmail.com>
2022-12-07 13:52:51 +0100

    a

---
+++ desktopowe/kabrioletDziedziczenie/kabrioletDziedziczenie/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace kabrioletDziedziczenie
+{
+    class Samochod
+    {
+        protected string marka;
+        protected int poj_baku;
+        protected int predkosc_max;
+        protected int zuzycia_paliwa;
+
+        public Samochod(string marka, int poj_baku, int predkosc_max, int zuzycia_paliwa)
+        {
+            this.marka = marka;
+            this.poj_baku = poj_baku;
+            this.predkosc_max = predkosc_max;
+            this.zuzycia_paliwa = zuzycia_paliwa;
+        }
+        public void Jedz(float jakSzybko, float jakDaleko)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
