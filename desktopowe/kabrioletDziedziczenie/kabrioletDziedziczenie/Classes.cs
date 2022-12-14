68ab5fa9bf9c328828a8603ac32717a230bd05b0
Okami_PL <szymon.galuszka.114@gmail.com>
2022-12-14 13:53:22 +0100

    a

---
+++ desktopowe/kabrioletDziedziczenie/kabrioletDziedziczenie/Classes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kabrioletDziedziczenie
+{
+    class Samochod
+    {
+        protected string marka;
+        protected int poj_baku;
+        protected int predkosc_max;
+        protected int zuzycia_paliwa;
+
+        public Samochod(string marka, int poj_baku, int predkosc_max, int zuzycia_paliwa)
+        {
+            this.marka = marka;
+            this.poj_baku = poj_baku;
+            this.predkosc_max = predkosc_max;
+            this.zuzycia_paliwa = zuzycia_paliwa;
+        }
+        public void Jedz(float jakSzybko, float jakDaleko)
+        {
+            if (jakSzybko > predkosc_max)
+            {
+                Console.WriteLine($"{marka} nie pojedzie szybciej niż {predkosc_max}");
+            }
+            else
+            {
+                Console.WriteLine($"{marka} pojedzie z prędkością {jakSzybko}, oraz będzie musiało tankować {(int)(jakDaleko / (poj_baku * 100)) / zuzycia_paliwa} razy");
+            }
+        }
+    }
+    class Kabriolet : Samochod
+    {
+        protected bool dach_otwarty;
+        public Kabriolet(string marka, int poj_baku, int predkosc_max, int zuzycia_paliwa, bool dach_otwarty) : base(marka, poj_baku, predkosc_max, zuzycia_paliwa)
+        {
+            this.marka = marka;
+            this.poj_baku = poj_baku;
+            this.predkosc_max = predkosc_max;
+            this.zuzycia_paliwa = zuzycia_paliwa;
+            this.dach_otwarty = dach_otwarty;
+        }
+        public void otworz_dach()
+        {
+            dach_otwarty = true;
+        }
+        public void zamknij_dach()
+        {
+            dach_otwarty = false;
+        }
+        public new void Jedz(float jakSzybko, float jakDaleko)
+        {
+            if (jakSzybko > predkosc_max)
+            {
+                Console.WriteLine($"{marka} nie pojedzie szybciej niż {predkosc_max}");
+            }
+            else
+            {
+                if (dach_otwarty)
+                {
+                    Console.WriteLine($"{marka} pojedzie z prędkością {jakSzybko}, oraz będzie musiało tankować {(int)(jakDaleko / (poj_baku * 100)) / (zuzycia_paliwa*1.15)} razy");
+                }
+                else
+                {
+                    Console.WriteLine($"Auto pojedzie z prędkością {jakSzybko}, oraz będzie musiało tankować {(int)(jakDaleko / (poj_baku * 100)) / zuzycia_paliwa} razy");
+                }
+            }
+        }
+    }
+}
--- desktopowe/kabrioletDziedziczenie/kabrioletDziedziczenie/Program.cs
+++ desktopowe/kabrioletDziedziczenie/kabrioletDziedziczenie/Program.cs
@@ -1,29 +1,16 @@
 ﻿namespace kabrioletDziedziczenie
 {
-    class Samochod
-    {
-        protected string marka;
-        protected int poj_baku;
-        protected int predkosc_max;
-        protected int zuzycia_paliwa;
-
-        public Samochod(string marka, int poj_baku, int predkosc_max, int zuzycia_paliwa)
-        {
-            this.marka = marka;
-            this.poj_baku = poj_baku;
-            this.predkosc_max = predkosc_max;
-            this.zuzycia_paliwa = zuzycia_paliwa;
-        }
-        public void Jedz(float jakSzybko, float jakDaleko)
-        {
-
-        }
-    }
-    internal class Program
+    class Program
     {
         static void Main()
         {
-            
+            Samochod mercedes = new Samochod("Mercedes", 10, 240, 10);
+            Kabriolet audi = new Kabriolet("Audi", 10, 210, 10, false);
+            mercedes.Jedz(250, 300);
+            mercedes.Jedz(200, 10); // ?????
+            audi.Jedz(180, 150);
+            audi.otworz_dach();
+            audi.Jedz(180, 150);
         }
     }
 }
