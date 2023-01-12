5d9199c15da7c48d47a352dba015b9a0a8184ecb
Okami_PL <szymon.galuszka.114@gmail.com>
2023-01-12 09:33:33 +0100

    do naprawy

--- desktopowe/wydawanieReszty/wydawanieReszty/MainWindow.xaml.cs
+++ desktopowe/wydawanieReszty/wydawanieReszty/MainWindow.xaml.cs
@@ -20,18 +20,19 @@ namespace wydawanieReszty
     /// </summary>
     public partial class MainWindow : Window
     {
+        static double[] defaultNominals = { 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01};
+        List<double> tab = new List<double>(defaultNominals);
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void giveChange_Click(object sender, RoutedEventArgs e)
         {
             outputListBox.Items.Clear();
             double change = double.Parse(inputTextBox.Text);
-            double[] tab = { 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01};
             
-            for(int i = 0; i < tab.Length; i++)
+            for(int i = 0; i < tab.Count; i++)
             {
                 if (tab[i] <= change)
                 {
@@ -40,7 +41,50 @@ namespace wydawanieReszty
                     outputListBox.Items.Add($"{amount} x {tab[i]}zł");
                 }
             }
-            
+        }
+
+        private void addNominal_Click(object sender, RoutedEventArgs e)
+        {
+            //double[] tab2 = new double[tab.Length];
+            /*for(int i = 0; i < tab.Length; i++)
+            {
+                if (double.Parse(nominalTextBox.Text) < tab[i])
+                {
+                    tab2[i] = double.Parse(nominalTextBox.Text);
+                }
+                else
+                {
+                    if(i == 0)
+                    {
+                        tab2[i] = tab[i];
+                    }
+                    else
+                    {
+                        tab2[i] = tab[i-1];
+                    }
+                }
+            }*/
+            /*for(int i = 0; i < tab2.Length; i++)
+            {
+                tab2[i] = tab[i];
+            }
+            tab2[tab2.Length - 1] = double.Parse(nominalTextBox.Text);
+            */
+            tab.Append<double>(double.Parse(nominalTextBox.Text));
+            for(int i = 0; i < tab.Count; i++)
+            {
+                for(int j = 0; j < tab.Count; j++)
+                {
+                    if (tab[i] > tab[j])
+                        (tab[i], tab[j]) = (tab[j], tab[i]);
+                }
+            }
+            nominalsLabel.Content = "(";
+            for(int i = 0; i < tab.Count; i++)
+            {
+                nominalsLabel.Content += $"{tab[i]}, ";
+            }
+            nominalsLabel.Content += ")zł";
         }
     }
 }
