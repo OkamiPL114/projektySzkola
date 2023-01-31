151a3d54e436cde6824f9c1fdf5e066c9ecb1630
Okami_PL <szymon.galuszka.114@gmail.com>
2023-01-31 10:04:06 +0100

    stworzenie listy

--- mobilne/ListViewApp/ListViewApp/ListViewApp/MainPage.xaml.cs
+++ mobilne/ListViewApp/ListViewApp/ListViewApp/MainPage.xaml.cs
@@ -5,14 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
 
 namespace ListViewApp
 {
     public partial class MainPage : ContentPage
     {
+        private ObservableCollection<Person> people;
         public MainPage()
         {
             InitializeComponent();
+            people = new ObservableCollection<Person>()
+            {
+                new Person(){ FirstName = "Jan", LastName = "Kowalski", Location="Jasło", Country="Polska", Gender=Gender.Male}
+            };
+            peopleListView.ItemsSource = people;
         }
     }
 }
