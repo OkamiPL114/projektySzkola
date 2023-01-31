5aec3dfff18dd6b6534bca5140293103fdac15b7
Okami_PL <szymon.galuszka.114@gmail.com>
2023-01-31 09:58:16 +0100

    dodatkowe pliki

---
+++ mobilne/ListViewApp/ListViewApp/ListViewApp/Gender.cs
@@ -0,0 +1,4 @@
+﻿enum Gender
+{
+    Male, Female, Other
+}
---
+++ mobilne/ListViewApp/ListViewApp/ListViewApp/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewApp
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string Country { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
