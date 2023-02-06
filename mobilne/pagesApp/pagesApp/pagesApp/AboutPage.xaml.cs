19a08f0cb85126db7ba00f49d31007f3c48ab616
Okami_PL <szymon.galuszka.114@gmail.com>
2023-02-06 11:25:23 +0100

    stworzenie projektu i początek

---
+++ mobilne/pagesApp/pagesApp/pagesApp/AboutPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pagesApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void backButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ mobilne/pagesApp/pagesApp/pagesApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pagesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
