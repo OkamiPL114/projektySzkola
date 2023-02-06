73f13911c917b124f1e033224983c025a2b07e83
Okami_PL <szymon.galuszka.114@gmail.com>
2023-02-06 19:10:10 +0100

    co to za pliki???

--- mobilne/ListViewUser/ListViewDemo/ListViewDemo.Android/obj/Debug/120/android/src/crc640b9ef597fc780c33/MainActivity.java
+++ mobilne/ListViewUser/ListViewDemo/ListViewDemo.Android/obj/Debug/120/android/src/crc640b9ef597fc780c33/MainActivity.java
@@ -20,16 +20,18 @@ public class MainActivity
 	public MainActivity ()
 	{
 		super ();
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("ListViewDemo.Droid.MainActivity, ListViewDemo.Android", "", this, new java.lang.Object[] {  });
+		}
 	}
 
 
 	public MainActivity (int p0)
 	{
 		super (p0);
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("ListViewDemo.Droid.MainActivity, ListViewDemo.Android", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
 	}
 
 
