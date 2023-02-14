1d6977b1953c97848b364314ceeb3475e0f95f56
Okami_PL <szymon.galuszka.114@gmail.com>
2023-02-14 09:52:52 +0100

    projekt obliczania kosztów paliwa

---
+++ mobilne/fuelCalc/fuelCalc/fuelCalc/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace fuelCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void showdPanelSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+    }
+}
