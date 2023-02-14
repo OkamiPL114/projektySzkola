09550d7c86f56b3e828d77edb4dcd29656b4ba20
Okami_PL <szymon.galuszka.114@gmail.com>
2023-02-14 09:21:20 +0100

    projekt z tabbed page

---
+++ mobilne/tabPage/tabPage/tabPage/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tabPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new IndexPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ mobilne/tabPage/tabPage/tabPage/IndexPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tabPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IndexPage : ContentPage
+    {
+        public IndexPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void appButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
