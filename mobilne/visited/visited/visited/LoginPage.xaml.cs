fa94888f0ade558ace5da07fb29666376a4a5801
Okami_PL <szymon.galuszka.114@gmail.com>
2023-02-21 08:47:02 +0100

    projekt wielostronnicowy

---
+++ mobilne/visited/visited/visited/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace visited
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void loginButton_ClickedAsync(object sender, EventArgs e)
+        {
+            var enteredPIN = pinEntry.Text;
+            foreach (var pin in PINs.Codes)
+            {
+                if(enteredPIN == pin)
+                {
+                    await Navigation.PushAsync(new MainPage());
+                    return;
+                }
+            }
+            await DisplayAlert("Błędny PIN", "Spróbuj jeszcze raz", "OK");
+        }
+    }
+}
---
+++ mobilne/visited/visited/visited/PINs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace visited
+{
+    static class PINs
+    {
+        public static List<string> Codes = new List<string>()
+        {
+            "0000", "1234", "1111"
+        };
+    }
+}
