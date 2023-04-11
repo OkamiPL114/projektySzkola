b65025c4ccede43eb3660f3a958d02c0887b1c41
Okami_PL <szymon.galuszka.114@gmail.com>
2023-04-11 20:05:21 +0200

    poprawne wyświetlanie ocen

--- mobilne/eJournal/eJournal/eJournal/Subject.cs
+++ mobilne/eJournal/eJournal/eJournal/Subject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace eJournal
 {
@@ -8,5 +9,25 @@ namespace eJournal
     {
         public string Name { get; set; }
         public List<int> Grades { get; set; }
+        private string _GradesStr;
+        public string GradesStr
+        {
+            get 
+            {
+                string temp = "";
+
+                foreach (var item in Grades)
+                {
+                    temp += $"{item}, ";
+                }
+                temp = temp.Substring(0, temp.Length - 2);
+                
+                return temp;
+            }
+            set
+            {
+                _GradesStr = value;
+            }
+        }
     }
 }
