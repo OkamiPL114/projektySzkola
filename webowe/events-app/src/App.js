26a21baea751ed91509484c0992677d6b2d40159
Okami_PL <szymon.galuszka.114@gmail.com>
2023-05-16 07:56:12 +0200

    events app

---
+++ webowe/events-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EventsTest from "./components/EventsTest";
+
+function App() {
+    return (
+        <React.Fragment>
+            <h1>Test</h1>
+            <EventsTest />
+        </React.Fragment>
+    )
+}
+
+export default App;
---
+++ webowe/events-app/src/components/EventsTest.js
@@ -0,0 +1,9 @@
+import "./EventsTest.css";
+
+function EventsTest() {
+    return (
+        <p>a</p>
+    )
+}
+
+export default EventsTest;
