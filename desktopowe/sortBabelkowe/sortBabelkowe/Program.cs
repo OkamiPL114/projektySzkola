dac7ccf0ea67d5e30e8ed437478d6813bf3a0025
Okami_PL <szymon.galuszka.114@gmail.com>
2023-10-19 14:36:04 +0200

    sortowanie bąbelkowe

---
+++ desktopowe/sortBabelkowe/sortBabelkowe/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace sortBabelkowe
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(0, 101);
+            }
+            sortujBabelkowo(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+        private static void sortujBabelkowo(int[] arr)
+        {
+            for(int i = 0; i < arr.Length;i++)
+            {
+                for(int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
