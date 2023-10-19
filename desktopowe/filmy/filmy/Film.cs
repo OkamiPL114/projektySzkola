7237d8006c663ba9e433500009a00a8b1b1d362f
Okami_PL <szymon.galuszka.114@gmail.com>
2023-10-19 14:28:31 +0200

    filmy

---
+++ desktopowe/filmy/filmy/Film.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmy
+{
+    class Film
+    {
+        private string tytul = "";
+        private int liczbaWypozyczen = 0;
+        public void ustawTytul(string tytul)
+        {
+            this.tytul = tytul;
+        }
+        public string pobierzTytul()
+        {
+            return this.tytul;
+        }
+        public int pobierzLiczbeWypozyczen()
+        {
+            return this.liczbaWypozyczen;
+        }
+        public void inkrementuj()
+        {
+            this.liczbaWypozyczen++;
+        }
+    }
+}
---
+++ desktopowe/filmy/filmy/Program.cs
@@ -0,0 +1,18 @@
+﻿using Filmy;
+
+namespace filmy
+{
+    class Program
+    {
+        static void Main()
+        {
+            Film f1 = new Film();
+            f1.ustawTytul("Harry Potter");
+            Console.WriteLine($"Tytuł: {f1.pobierzTytul()}");
+
+            Console.WriteLine($"{f1.pobierzLiczbeWypozyczen()}");
+            f1.inkrementuj();
+            Console.WriteLine($"{f1.pobierzLiczbeWypozyczen()}");
+        }
+    }
+}
