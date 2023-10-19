bfbe75a75a703302322c8d561cb429f0bb73761d
Okami_PL <szymon.galuszka.114@gmail.com>
2023-10-19 13:47:43 +0200

    czy pierwsza

---
+++ desktopowe/czyPierwsza/czyPierwsza/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace czyPierwsza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę: ");
+            int.TryParse(Console.ReadLine(), out var x);
+            if (czy_liczba_pierwsza(x))
+            {
+                Console.WriteLine($"Liczba {x} jest liczbą pierwszą");
+            }
+            else
+            {
+                Console.WriteLine($"Liczba {x} nie jest liczbą pierwszą");
+            }
+        }
+
+        private static bool czy_liczba_pierwsza(int x)
+        {
+            if(x == 2) return true;
+            if(x == 3) return true;
+            for (int i = 2; i < 10; i++)
+            {
+                if(x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
