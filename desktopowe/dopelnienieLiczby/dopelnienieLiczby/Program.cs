e8296031699bcf6dad45d95952cceac9f4fa8989
Okami_PL <szymon.galuszka.114@gmail.com>
2023-11-09 13:33:14 +0100

    dopełnienie liczby

---
+++ desktopowe/dopelnienieLiczby/dopelnienieLiczby/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace dopelnienieLiczby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę całkowitą: ");
+            int.TryParse(Console.ReadLine(), out var number);
+            int place = 1;
+            int complement = 0;
+            while (number > 0)
+            {
+                int compNum = 9 - (number % 10);
+                number = number / 10;
+                complement += compNum * place;
+                place *= 10;
+            }
+            Console.WriteLine($"Dopełnieniem liczby {number} jest {complement}");
+        }
+    }
+}
