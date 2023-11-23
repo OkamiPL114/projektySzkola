64e4a7fb1deb4751d5fb627bce09e2e9d8a29172
Okami_PL <szymon.galuszka.114@gmail.com>
2023-11-23 15:58:46 +0100

    symulacja szkoły
    kurwa, pojebie mnie ten binary search

---
+++ desktopowe/ocenySzkolne/ocenySzkolne/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Channels;
+
+namespace ocenySzkolne
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Student Andrzej = new Student("Andrzej Barzyk");
+            Student Anna = new Student("Anna Gwelowska");
+            Student Wiktor = new Student("Wiktor Gajda");
+            Student Marysia = new Student("Marysia Wieczysta");
+
+            Andrzej.AddGrade(4);
+            Andrzej.AddGrade(1);
+            Andrzej.AddGrade(4);
+            Andrzej.AddGrade(3);
+            Andrzej.AddGrade(5);
+            Andrzej.AddGrade(5);
+
+            Console.WriteLine(Andrzej.Grades[0]);
+            Console.WriteLine(Andrzej.Grades[1]);
+            Console.WriteLine(Andrzej.Grades[2]);
+            Console.WriteLine(Andrzej.Grades[3]);
+            Console.WriteLine(Andrzej.Grades[4]);
+            Console.WriteLine(Andrzej.Grades[5]);
+
+            int searched = 3;
+            if (Andrzej.BinarySearchGrade(searched))
+            {
+                Console.WriteLine($"Jest ocena {searched} w dzienniku");
+            }
+            else
+            {
+                Console.WriteLine($"Nie ma oceny {searched} w dzienniku");
+            }
+            searched = 6;
+            if (Andrzej.BinarySearchGrade(searched))
+            {
+                Console.WriteLine($"Jest ocena {searched} w dzienniku");
+            }
+            else
+            {
+                Console.WriteLine($"Nie ma oceny {searched} w dzienniku");
+            }
+        }
+    }
+}
---
+++ desktopowe/ocenySzkolne/ocenySzkolne/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocenySzkolne
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public List<int> Grades;
+        public Student(string name) 
+        {
+            this.Name = name;
+            Grades = new List<int>();
+        }
+        public void AddGrade(int grade)
+        {
+            Grades.Add(grade);
+            for(int i = 0; i < Grades.Count; i++)
+            {
+                for(int j = 0; j < Grades.Count - 1; j++)
+                {
+                    if (Grades[i] < Grades[j])
+                    {
+                        (Grades[i], Grades[j]) = (Grades[j], Grades[i]);
+                    }
+                }
+            }
+        }
+        public bool BinarySearchGrade(int searched)
+        {
+            int left = 0;
+            int right = Grades[Grades.Count - 1];
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+                if(Grades[mid] == searched)
+                {
+                    return true;
+                }
+                else if(Grades[mid] > searched)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return false;
+        }
+    }
+}
