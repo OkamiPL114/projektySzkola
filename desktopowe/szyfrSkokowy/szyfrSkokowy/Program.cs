7c78c3f90bb21e63a81efa529cb4104b0616c907
Okami_PL <szymon.galuszka.114@gmail.com>
2023-11-23 13:39:58 +0100

    szyfr skokowy

---
+++ desktopowe/szyfrSkokowy/szyfrSkokowy/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace szyfrSkokowy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj wyraz do zaszyfrowania: ");
+            string word = Console.ReadLine();
+            Console.Write("Podaj przesunięcie wyrazu: ");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            int n = word.Length;
+            int m = n / k;
+            if(n % k != 0)
+            {
+                m += 1;
+            }
+            Console.Write("Zaszyfrowane słowo to: ");
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = i; j < n; j += m)
+                {
+                    Console.Write(word[j]);
+                }
+            }
+        }
+    }
+}
