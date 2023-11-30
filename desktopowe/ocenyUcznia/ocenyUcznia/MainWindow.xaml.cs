a4079b52719c6a00b38b42e3df5098bbaa5b727b
Okami_PL <szymon.galuszka.114@gmail.com>
2023-11-30 15:58:13 +0100

    oceny ucznia

--- desktopowe/ocenyUcznia/ocenyUcznia/MainWindow.xaml.cs
+++ desktopowe/ocenyUcznia/ocenyUcznia/MainWindow.xaml.cs
@@ -21,7 +21,6 @@ namespace ocenyUcznia
     public partial class MainWindow : Window
     {
         List<Mark> marks = new List<Mark>();
-        List<Mark> sortedMarks = new List<Mark>();
         public MainWindow()
         {
             InitializeComponent();
@@ -52,34 +51,71 @@ namespace ocenyUcznia
                 marksLabel.Content += $", {mark}";
             }
             marksLabel.Visibility = Visibility.Visible;
+            markTextBox.Text = "";
+            weightTextBox.Text = "";
         }
         public void calcArytAverage()
         {
-            int average = 0;
+            double average = 0;
             for (int i = 0; i < marks.Count; i++)
             {
                 average += marks[i].mark;
             }
             average = average / marks.Count;
+            average = Math.Round(average, 2);
             averageArytLabel.Content = $"Średnia arytmetyczna ocen: {average}";
             averageArytLabel.Visibility = Visibility.Visible;
         }
         public void calcWeigAverage()
         {
-            int average = 0;
+            bubbleSort();
+            double average = 0;
             int devider = 0;
             for(int i = 0; i < marks.Count; i++)
             {
-                average += marks[i].mark;
+                average += marks[i].mark * marks[i].weight;
                 devider += marks[i].weight;
             }
-            average = average / devider;
+            average /= devider;
+            average = Math.Round(average, 2);
             averageWeigLabel.Content = $"Średnia ważona ocen: {average}";
             averageWeigLabel.Visibility = Visibility.Visible;
         }
         public void findMedian()
         {
-
+            double median = 0;
+            if(marks.Count % 2 == 0)
+            {
+                double first = marks[(marks.Count / 2) - 1].mark;
+                double second = marks[marks.Count / 2].mark;
+                median = (first + second) / 2;
+            }
+            else
+            {
+                if(marks.Count == 1)
+                {
+                    median = marks[0].mark;
+                }
+                else
+                {
+                    median = marks[(marks.Count / 2)].mark;
+                }
+            }
+            medianLabel.Content = $"Mediana ocen: {median}";
+            medianLabel.Visibility = Visibility.Visible;
+        }
+        public void bubbleSort()
+        {
+            for(int i = 0; i < marks.Count; i++)
+            {
+                for(int j = 0; j < marks.Count; j++)
+                {
+                    if(marks[i].mark < marks[j].mark)
+                    {
+                        (marks[i], marks[j]) = (marks[j], marks[i]);
+                    }
+                }
+            }
         }
     }
 }
--- desktopowe/ocenyUcznia/ocenyUcznia/obj/Debug/net6.0-windows/App.g.i.cs
+++ desktopowe/ocenyUcznia/ocenyUcznia/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ocenyUcznia {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ocenyUcznia {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public static void Main() {
             ocenyUcznia.App app = new ocenyUcznia.App();
             app.InitializeComponent();
