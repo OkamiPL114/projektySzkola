fee702018e26f8b03b6cca3a650512b151b92720
Okami_PL <szymon.galuszka.114@gmail.com>
2023-12-14 14:41:43 +0100

    do skończenia

---
+++ desktopowe/nPermutacja/nPermutacja/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace nPermutacja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 1 };
+            int n = arr.Length;
+            int sum = 0;
+            int arrSum = 0;
+            for(int i = 0; i < n; i++) // sprawdź czy ciąg nie jest już permutacją
+            {
+                sum += i + 1;
+                arrSum += arr[i];
+            }
+            if(arrSum == sum)
+            {
+                Console.WriteLine($"Ciąg jest już permutacją liczby {n}");
+                return;
+            }
+            int[] goodArr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                goodArr[i] = i + 1;
+            }
+            for(int i = 0; i < n; i++)
+            {
+                if(arr[i] == goodArr[i]) goodArr[i] = 0;
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
